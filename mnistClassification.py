8f54f6251ca905ec1a527b57eba565511a7ade0f
zyl <zylzlh0330@163.com>
2018-03-28 05:49:38 +0800

    Saver

--- mnistClassification.py
+++ mnistClassification.py
@@ -1,19 +1,24 @@
 #coding:utf-8
+
+isTrain = False 
+train_steps = 1100  
+checkpoint_steps = 50
+Mini_batch=100
+
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 mnist=input_data.read_data_sets('MNIST_data',one_hot=True)
 
-def layer(inputs,in_size,out_size,n_layer,activation_function=None):
-    layer_name='layer%s'%n_layer
-    Weights=tf.Variable(tf.random_normal([in_size,out_size]))
-    biases=tf.Variable(tf.zeros([1,out_size])+0.1)
+def layer(inputs,in_size,out_size,activation_function=None):
+    Weights=tf.Variable(tf.truncated_normal([in_size,out_size],stddev=0.1)*tf.sqrt(1/in_size),
+                        name='Weights')
+    biases=tf.Variable(tf.random_normal([1,out_size]),name='biases')
     Wx_plus_b=tf.add(tf.matmul(inputs,Weights),biases)
     if activation_function:
         outputs=Wx_plus_b
     else:
         outputs=activation_function(Wx_plus_b)
     return outputs
-
 
 def compute_accuracy(v_xs,v_ys):
     global prediction
@@ -25,20 +30,31 @@ def compute_accuracy(v_xs,v_ys):
 
 xs=tf.placeholder(tf.float32,[None,784])
 ys=tf.placeholder(tf.float32,[None,10])
-prediction=layer(xs,784,10,2,activation_function=tf.nn.softmax)
+prediction=layer(xs,784,10,activation_function=tf.nn.softmax)
 cross_entropy=tf.reduce_mean(-tf.reduce_sum(ys*tf.log(prediction),
 reduction_indices=[1]))
-train_step=tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+train_step=tf.train.GradientDescentOptimizer(0.3).minimize(cross_entropy)
+saver=tf.train.Saver(max_to_keep=1)
 
 with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    
-    for i in range(1000):
-        batch_xs,batch_ys=mnist.train.next_batch(100)
-        sess.run(train_step,feed_dict={xs:batch_xs,ys:batch_ys})
-        if i%50==0:
-            print(compute_accuracy(
-                mnist.test.images,mnist.test.labels))
+    if isTrain:
+        sess.run(tf.global_variables_initializer())
+        max_acc=0
+        for i in range(train_steps):
+            batch_xs,batch_ys=mnist.train.next_batch(Mini_batch)
+            sess.run(train_step,feed_dict={xs:batch_xs,ys:batch_ys})
+            if i%checkpoint_steps==0:
+                accuracy=compute_accuracy(mnist.test.images,mnist.test.labels)
+                print(accuracy)
+                if accuracy>max_acc:
+                    max_acc=accuracy
+                    saver.save(sess,r'C:\Users\zylzlh\Desktop\my_net\save_net.ckpt',global_step=i+1)
+    #else:
+        #model_file=tf.train.latest_checkpoint(r'C:\Users\zylzlh\Desktop\my_net\save_net.ckpt')
+        #saver.restore(sess,model_file)
+        #print( sess.run(prediction,feed_dict={xs:mnist.train.next_batch(10)}))
+        #print(sess.run(Weights))  
+        #print(sess.run(biases))
             
             
             
