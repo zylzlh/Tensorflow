40bda8e270c2c1f709caa3990a6a5fc2b89a21d3
zyl <zylzlh0330@163.com>
2018-03-29 10:05:15 +0800

    tensorboard_up

--- visualization.py
+++ visualization.py
@@ -7,43 +7,31 @@ Created on Mon Mar 26 11:44:33 2018
 
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
-def add_layer(inputs,in_size,out_size,activation_function=None):
-    Weights=tf.Variable(tf.random_normal([in_size,out_size]))
-    biases=tf.Variable(tf.zeros([1,out_size])+0.1)
-    Wx_plus_b=tf.matmul(inputs,Weights)+biases
-    if activation_function==None:
-        outputs=Wx_plus_b
-    else:
-        outputs=activation_function(Wx_plus_b)
-    return outputs
 
 x_data=np.linspace(-1,1,300,dtype=np.float32)[:,np.newaxis]
 noise=np.random.normal(0,0.05,x_data.shape).astype(np.float32)
 y_data=np.square(x_data)+noise-0.5
-fig=plt.figure()
-ax=fig.add_subplot(1,1,1)
-ax.scatter(x_data,y_data)
 
-xs=tf.placeholder(tf.float32,[None,1])
-ys=tf.placeholder(tf.float32,[None,1])
+with tf.variable_scope('Inputs'):
+    xs=tf.placeholder(tf.float32,[None,1],name='x')
+    ys=tf.placeholder(tf.float32,[None,1],name='y')
 
-l1=add_layer(xs,1,10,activation_function=tf.nn.relu)
-prediction=add_layer(l1,10,1)
+with tf.variable_scope('Net'):
+    l1=tf.layers.dense(xs,10,tf.nn.relu,name='hidden_layer')
+    prediction=tf.layers.dense(l1,1,name='output_layer')
+    #add to histgram summary
+    tf.summary.histgram('h_out',l1)
+    tf.summary.histgram('pred',prediction)
 
-loss=tf.reduce_mean(tf.reduce_sum(tf.square(ys-prediction),
-    reduction_indices=[1]))
+loss=tf.mean_squared_error(ys,prediction,scope='loss')
 train_step=tf.train.GradientDescentOptimizer(0.2).minimize(loss)
+tf.summary.scalar('loss',loss)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
+    writer=tf.summary.FileWriter(r'C:\log',sess.graph)
+    merge_op=tf.summary.merge_all()
     for i in range(1000):
-        sess.run(train_step,feed_dict={xs:x_data,ys:y_data})
-        if i%50==0:
-            try:
-                ax.lines.remove(lines[0])
-            except:
-                pass
-            prediction_value=sess.run(prediction,feed_dict={xs:x_data})
-            lines=ax.plot(x_data,prediction_value,'r-',lw=5)
-            plt.pause(0.1)
+        _,result=sess.run([train_step,merge_op],feed_dict={xs:x_data,ys:y_data})
+        writer.add_summary(result,i)
+        
